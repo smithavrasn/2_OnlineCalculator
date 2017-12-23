c8004104e3d39989de55eb1081060846fb44eee8
Vijaykumar <Smitha.Vijaykumar@ellucian.com>
2017-12-23 15:58:27 +0530

    ADD

--- src/test/java/com/demo/example/CalculatorTest.java
+++ src/test/java/com/demo/example/CalculatorTest.java
@@ -16,9 +16,13 @@ public class CalculatorTest {
 	}
 
 	@Test
-	public void test() {
+	public void addtest() {
 		int result=obj.add(2,3);
 		assertEquals(5,result);
+	}
+	public void subtest() {
+		int result=obj.add(2,3);
+		assertEquals(1,result);
 	}
 
 }
